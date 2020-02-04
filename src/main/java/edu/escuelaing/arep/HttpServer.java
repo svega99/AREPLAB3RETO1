02b51b4bedfd24c7783a86fd14ba134f6df5c3a5
unknown <santiago.vega-r@labinfo.is.escuelaing.edu.co>
2020-02-04 13:15:55 -0500

    ing

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -1,50 +1,132 @@
 package edu.escuelaing.arep;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.*;
 
 public class HttpServer {
+	static ServerSocket serverSocket = null;
+	
+	static Socket clientSocket = null;
+	
+	static PrintWriter out;
+	static BufferedReader in;
+	
+	
   public static void main(String[] args) throws IOException {
-   ServerSocket serverSocket = null;
+   serverSocket = null;
    try { 
       serverSocket = new ServerSocket(35000);
    } catch (IOException e) {
       System.err.println("Could not listen on port: 35000.");
       System.exit(1);
    }
+   while(true) {
+   
 
-   Socket clientSocket = null;
-   try {
-       System.out.println("Listo para recibir ...");
-       clientSocket = serverSocket.accept();
-   } catch (IOException e) {
-       System.err.println("Accept failed.");
-       System.exit(1);
+	   
+	   try {
+	       System.out.println("Listo para recibir ...");
+	       clientSocket = serverSocket.accept();
+	   } catch (IOException e) {
+	       System.err.println("Accept failed.");
+	       System.exit(1);
+	   }
+	   out = new PrintWriter(
+	                         clientSocket.getOutputStream(), true);
+	   in = new BufferedReader(
+	                         new InputStreamReader(clientSocket.getInputStream()));
+	   String inputLine, outputLine;
+	   
+	   StringBuilder stringBuilder = new StringBuilder();
+	   
+	   Pattern pattern = Pattern.compile("GET /([^\\s]+)");
+       Matcher matcher = null;
+	   
+	   while ((inputLine = in.readLine()) != null) {
+	      System.out.println("Recibí: " + inputLine);
+	      stringBuilder.append(inputLine);
+	      if (!in.ready()) {
+	    	  matcher = pattern.matcher(stringBuilder.toString());
+              if (matcher.find()) {
+                  String req = matcher.group().substring(5);
+                  System.out.println("VALUE: " + req);
+                  returnRequest(req);
+              }
+	    	  
+	    	  break; }
+	   }
+	  
+	    out.close(); 
+	    in.close(); 
+	    clientSocket.close(); 
+	    //serverSocket.close();
    }
-   PrintWriter out = new PrintWriter(
-                         clientSocket.getOutputStream(), true);
-   BufferedReader in = new BufferedReader(
-                         new InputStreamReader(clientSocket.getInputStream()));
-   String inputLine, outputLine;
-   while ((inputLine = in.readLine()) != null) {
-      System.out.println("Recibí: " + inputLine);
-      if (!in.ready()) {break; }
-   }
-   outputLine = 
-          "<!DOCTYPE html>" + 
-          "<html>" + 
-          "<head>" + 
-          "<meta charset=\"UTF-8\">" + 
-          "<title>Title of the document</title>\n" + 
-          "</head>" + 
-          "<body>" + 
-          "<h1>Mi propio mensaje</h1>" + 
-          "</body>" + 
-          "</html>" + inputLine; 
-    out.println(outputLine);
-    out.close(); 
-    in.close(); 
-    clientSocket.close(); 
-    serverSocket.close(); 
   }
+  
+  public static void returnRequest(String req) throws IOException {
+	  
+	  
+	  String path = "src/main/resources/";
+      String ext = FilenameUtils.getExtension(req);
+      if (ext.equals("js")) {
+    	  path=path+"js/";
+    	  
+      }else if (ext.equals("png") || ext.equals("jpg")) {
+    	  path=path+"img/";
+      }
+      
+      
+      File file = new File(path+req);
+      
+      if (file.exists() && !file.isDirectory()) {
+	      if (ext.equals("png") || ext.equals("jpg")) {
+	    	  	
+	    	  	
+				FileInputStream fis = new FileInputStream(file);
+				byte[] data = new byte[(int) file.length()];
+				fis.read(data);
+				fis.close();
+	                      
+	             // Cabeceras con la info de la imágen
+				DataOutputStream binaryOut = new DataOutputStream(clientSocket.getOutputStream());
+				binaryOut.writeBytes("HTTP/1.0 200 OK\r\n");
+				binaryOut.writeBytes("Content-Type: image/"+ext+"\r\n");
+				binaryOut.writeBytes("Content-Length: " + data.length);
+				binaryOut.writeBytes("\r\n\r\n");
+				binaryOut.write(data);
+	
+				binaryOut.close();
+	    	  
+	      }
+	      else {
+	    	  DataOutputStream binaryOut = new DataOutputStream(clientSocket.getOutputStream());
+				binaryOut.writeBytes("HTTP/1.0 200 OK\r\n");
+				binaryOut.writeBytes("Content-Type: text/html");
+				binaryOut.writeBytes("\r\n\r\n");
+				
+				binaryOut.close();
+	    	  
+		    	  BufferedReader br = new BufferedReader(new FileReader(file));
+	
+	              StringBuilder stringBuilder = new StringBuilder();
+	              String st;
+	              while ((st = br.readLine()) != null) {
+	                  stringBuilder.append(st);
+	              }
+	              out.println(stringBuilder.toString());
+				
+	      }
+      }
+      else {
+    	  out.println("HTTP/1.1 404 \r\n\r\n<html><body><h1>ERROR 404: NOT FOUND</h1></body></html>");
+    	  
+      }
+	  
+  }
+  
+  
 }
